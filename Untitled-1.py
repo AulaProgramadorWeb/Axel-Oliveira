e7975eee27c687cd0beb09227ad2d551fd4cd46f
axel051 <axeloliveiravieira@gmail.com>
2023-05-16 11:26:22 -0300

    exercicios 16/05

---
+++ Untitled-1.py
@@ -0,0 +1,21 @@
+#tipos de váriaveis
+#int/float/str/
+#comando: print()
+
+idade = int(input("Diga qual a sua idade:" ))16
+idade + idade + 5
+print("Sua idade será em cinco anos", idade)
+
+
+  #exercicio 1  
+
+def área(largura, altura):
+    a = largura * altura
+    print(l'A área de um terreno {largura} x {altura} é de {a}m² . ')
+
+    #calculo
+    print('Controle de Terrenos')
+    print('-' * 20)
+    l = float(input('Largura (m):'))
+    a = float(input('Altura (m):'))
+    área(l,a)
